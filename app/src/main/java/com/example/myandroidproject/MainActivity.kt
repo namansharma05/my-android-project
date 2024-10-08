c3689147a15f12dc5516d91c8b0b526ca3b2a3f3
Naman Sharma <namanks0511@gmail.com>
2024-10-08 16:43:24 +0530

    use of textView

--- app/src/main/java/com/example/myandroidproject/MainActivity.kt
+++ app/src/main/java/com/example/myandroidproject/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.example.myandroidproject
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var myText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+        myText = findViewById(R.id.textExample)
+
+        myText.setTextColor(Color.BLACK)
+        myText.setBackgroundColor(Color.RED)
+
+        myText.setOnClickListener {
+            myText.setTextColor(Color.WHITE)
+            myText.setBackgroundColor(Color.BLACK)
         }
     }
 }
